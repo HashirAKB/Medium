361bea44f52a251c3a430b4c828e3e62aeed8ce4
HashirAKB <ahmedhashir96@gmail.com>
2024-08-22 06:56:18 +0530

    Routing and Layout setup.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,30 @@
-import { Button } from "@/components/ui/button"
 import './App.css'
+import { Toaster } from "@/components/ui/toaster"
+import SignUp from './pages/SignupForm'
+import SignIn from './pages/SigninForm'
+import Navbar from './components/NavBar'
+import Home from './pages/LandingPage'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { AuthProvider } from './utils/AuthContext'
+// import { ProtectedRoute } from './components/ProtectedRoutes'
+
 
 function App() {
 
   return (
-    <>
-      <Button>Click me</Button>
-    </>
+    <AuthProvider>
+      <BrowserRouter>
+        <div className="flex flex-col h-screen">
+          <Navbar />
+          <Toaster/>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/signup" element={<SignUp />}/>
+            <Route path="/signin" element={<SignIn />}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
