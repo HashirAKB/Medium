e95b4066a6c17fac6d18ad34a295a6df76fcfd15
HashirAKB <ahmedhashir96@gmail.com>
2024-08-28 21:51:38 +0530

    Removed dummy blogs.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -24,7 +24,7 @@ const App: React.FC = () => {
             <Route path="/signup" element={<SignUp />}/>
             <Route path="/signin" element={<SignIn />}/>
             <Route element={<ProtectedRoute />}>
-              <Route path="/feed" element={<BlogFeed posts={dummyBlogs}/>}/>
+              <Route path="/feed" element={<BlogFeed/>}/>
               <Route path="/post/:id" element={<FullBlogPost />}/>
               <Route path="/create" element={<CreateBlog />}/>
               <Route path="/profile" element={<ProfileComponent />}/>
